e93ec83f5906ee98e8d67acedbb41dab11fee930
Movar <movsar.kh.128@gmail.com>
2020-08-06 00:55:34 +0300

    1010

--- js/main.js
+++ js/main.js
@@ -22,17 +22,15 @@ $(document).ready(function () {
       prevEl: ".reviews-slider__button--prev",
     },
     keyboard: { enabled: !0, onlyInViewport: !1 },
-  });
+	});
+	
   document.querySelector(".menu-button").addEventListener("click", function () {
-    console.log(
-      "\u041a\u043b\u0438\u043a \u043f\u043e \u043a\u043d\u043e\u043f\u043a\u0435 \u043c\u0435\u043d\u044e"
-    );
     document
       .querySelector(".navbar-bottom")
       .classList.toggle("navbar-bottom_visible");
   });
   var d = $("[data-toggle=modal]"),
-    e = $(".modal__close");
+    	e = $(".modal__close");
   d.on("click", function () {
     var a = $(this).attr("data-href");
     $(a).find(".modal__overlay").addClass("modal__overlay--visible");
@@ -41,17 +39,8 @@ $(document).ready(function () {
   e.on("click", c);
   window.onkeydown = function () {
     "Escape" == event.key && c();
-  };
-  // ymaps.ready(function () {
-  //   var a = new ymaps.Map("map", {
-  //       center: [12.934923151175724, 100.88307986706273],
-  //       zoom: 15,
-  //     }),
-  //     b = new ymaps.Placemark([12.934923151175724, 100.88307986706273], {
-  //       hintContent: "Hilton Hotel",
-  //     });
-  //   a.geoObjects.add(b);
-  // });
+	};
+	
   $('[name="phone"]').on("input", function () {
     $(this).val(
       $(this)
@@ -81,19 +70,11 @@ $(document).ready(function () {
 	$(".phone").mask("+7(999) 999-99-99");
 	
 	// $(".newsletter-parallax").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
-	AOS.init();
 	AOS.init({
 		disable: function () {
 			return 768 > window.innerWidth;
 		},
 	});
-
-	// [].forEach.call(document.querySelectorAll("img[data-src]"), function (img) {
-	// 	img.setAttribute("src", img.getAttribute("data-src"));
-	// 	img.onload = function () {
-	// 		img.removeAttribute("data-src");
-	// 	};
-	// });
 });
 
 
